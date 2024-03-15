2b2a095802268fcc4b8038aac85f304f4d52ce01
Bhargav Joshi <68766403+bhargavjoshi1237@users.noreply.github.com>
2024-03-15 09:37:24 +0000

    ok

---
+++ my-api-name/src/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const redis = require('redis');
+require('dotenv').config();
+
+const middlewares = require('./middlewares');
+const api = require('./api');
+
+const app = express();
+const redisClient = redis.createClient();
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get('/:key', (req, res) => {
+  const { key } = req.params;
+
+  // Fetch the value from Redis
+  redisClient.get(key, (err, value) => {
+      if (err) {
+          console.error('Error fetching from Redis:', err);
+          res.status(500).send('Error fetching from Redis');
+      } else if (value === null) {
+          res.status(404).send('Key not found in Redis');
+      } else {
+          res.send(value);
+      }
+  });
+});
+
+app.use('/api/v1', api);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+module.exports = app;
